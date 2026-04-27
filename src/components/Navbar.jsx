625e68bfb71b8dec9d9cd5bd7cdf47981f1487c3
priyanshu navidolatwala <pnavidolatwala@gmail.com>
2026-04-27 14:57:17 +0530

    count badge added in cart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,11 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ShoppingBag, ShoppingCart } from "lucide-react";
 import image from "/public/bag.webp";
+import { useCartContext } from "../context/CartContext";
 
 const Navbar = () => {
+  const { cart } = useCartContext();
+  const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
   return (
     <nav className="flex justify-between p-4 fixed top-0 left-0 w-full bg-gray-900/90 backdrop-blur shadow-md text-white z-100">
       <Link to="/">
@@ -16,11 +19,18 @@ const Navbar = () => {
           ShopEasy
         </div>
       </Link>
-      <span className="flex items-center font-bold text-xl mr-5">
-        <Link to="/cart">Cart</Link>
-        <Link to="/cart">
-          <ShoppingCart className="size-6.5 pl-1 pt-0.5" />
+
+      <span className="relative flex items-center font-bold text-xl mr-5">
+        <Link to="/cart" className="flex items-center">
+          Cart
+          <ShoppingCart className="size-6 ml-1" />
         </Link>
+
+        {totalItems > 0 && (
+          <span className="absolute -top-2 -right-3 bg-red-500 text-white text-xs font-bold px-1.5 py-0 mt-1 mr-1 rounded-full">
+            {totalItems}
+          </span>
+        )}
       </span>
     </nav>
   );
