345914b79362ed99675252cc6469236dee1063ac
priyanshu navidolatwala <pnavidolatwala@gmail.com>
2026-06-10 18:45:49 +0530

    categories added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
+import "../index.css";
+import { data } from "react-router-dom";
 
 const Home = ({ search }) => {
   const [products, setProducts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("all");
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
@@ -14,14 +17,42 @@ const Home = ({ search }) => {
     return <p className="p-4 h-screen">Loading products...</p>;
   }
 
-  const filteredProducts = products.filter((product) =>
-    product.title.toLowerCase().includes(search.toLowerCase()),
-  );
+  const filteredProducts = products.filter((product) => {
+    const matchesSearch = product.title
+      .toLowerCase()
+      .includes(search.toLowerCase());
+
+    const matchesCategory =
+      selectedCategory === "all" || product.category === selectedCategory;
+
+    return matchesSearch && matchesCategory;
+  });
+
+  const categories = [
+    "all",
+    ...new Set(products.map((product) => product.category)),
+  ];
 
   return (
     <>
+      <div className="flex gap-2 mb-6 overflow-x-auto scrollbar-hide md:justify-center">
+        {categories.map((category) => (
+          <button
+            key={category}
+            onClick={() => setSelectedCategory(category)}
+            className={`px-4 py-2 rounded-full whitespace-nowrap transition ${
+              selectedCategory === category
+                ? "bg-sky-600 text-white"
+                : "bg-white text-gray-800 hover:bg-gray-200"
+            }`}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+
       <div className="flex md:justify-center bg-gray-100 top-0 pb-6">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 p-4 max-w-7xl bg-gray-100">
+        <div className="grid grid-cols-1 w-full sm:grid-cols-2 md:grid-cols-4 gap-4 p-4 max-w-7xl bg-gray-100">
           {filteredProducts.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
