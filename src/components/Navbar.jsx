cf3522852db9fe37fcf4240da9dc9ee37c5a43c1
priyanshu navidolatwala <pnavidolatwala@gmail.com>
2026-06-10 17:59:00 +0530

    search bar & footer added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,58 +26,6 @@ const Navbar = ({ search, setSearch }) => {
   }, [totalItems]);
 
   return (
-    // <nav className="flex justify-between gap-5 items-center p-3 fixed top-0 left-0 w-full bg-gray-900/90 backdrop-blur shadow-md text-white z-100">
-    //   <Link to="/">
-    //     <div className="flex justify-center font-bold text-xl items-center ">
-    //       <img
-    //         src={image}
-    //         alt="img"
-    //         className="object-contain h-5.5 m-1 inline-block ml-4"
-    //       />
-    //       ShopEasy
-    //     </div>
-    //   </Link>
-
-    //   <div className="flex gap-5 md:w-full max-w-2xl ">
-    //     <input
-    //       type="text"
-    //       placeholder="Search products..."
-    //       value={search}
-    //       onChange={(e) => setSearch(e.target.value)}
-    //       className="hidden md:block w-full px-4 py-2 border border-white rounded-md text-slate-800 bg-white"
-    //     />
-    //     <span className="relative flex items-center font-bold text-xl">
-    //       <Link to="/cart" className="flex items-center">
-    //         Cart
-    //         <ShoppingCart className="size-6 ml-1" />
-    //       </Link>
-
-    //       {totalItems > 0 && (
-    //         <span
-    //           className={`absolute -top-2 -right-3 bg-red-500 text-white text-xs font-semibold px-1.5 mt-1 mr-0.5 rounded-full ${animate ? "scale-110" : ""} transition`}
-    //         >
-    //           {totalItems}
-    //         </span>
-    //       )}
-    //     </span>
-
-    //     {user ? (
-    //       <button
-    //         onClick={logout}
-    //         className="ml-2 bg-red-500 hover:bg-red-600 px-3 py-1 rounded"
-    //       >
-    //         Logout
-    //       </button>
-    //     ) : (
-    //       <Link
-    //         to="/login"
-    //         className="ml-2 bg-sky-600 hover:bg-sky-700 px-3 py-1 rounded"
-    //       >
-    //         Login
-    //       </Link>
-    //     )}
-    //   </div>
-    // </nav>
     <nav className="fixed top-0 left-0 w-full bg-gray-900/90 backdrop-blur shadow-md text-white z-50">
       <div className="px-4 py-3">
         {/* Top Row */}
